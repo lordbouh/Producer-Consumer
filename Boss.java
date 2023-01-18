f70d153cb0f5767ee434009a8125bead0d10cdf1
lordbouh <lordbouh@yahoo.com>
2023-01-18 18:13:37 +0400

    Created

---
+++ Boss.java
@@ -0,0 +1,12 @@
+import java.util.Queue;
+
+public class Boss {
+    private Queue toDoQueue;// у босса тоже есть ссылка на очередь из заданий
+    public Boss(Queue toDoQueue) {
+        this.toDoQueue = toDoQueue;// даем ссылку на очередь в конструкторе
+    }
+    public void giveTask(String task) {
+        toDoQueue.add(task); // кладем задание в очередь
+        System.out.println("Дал задачу " + task);
+    }
+}
